af66a4f259057123f9e7ca9d2d544bb9e7d9645a
mono-1729 <kawai.172957@gmail.com>
2023-06-08 12:32:15 +0900

    combination

--- 備忘録/組み合わせの数.py
+++ 備忘録/組み合わせの数.py
@@ -1,14 +1,11 @@
-n=int(input())
-a=[1,1]
-b=[]
-for i in range(2,n+2):
-  a.append(a[i-1]*i%1000000007)#階乗を求める
-for i in range(n+1):
-  b.append(pow(a[i],1000000005,1000000007))#1/a[i]の逆元を計算(x/a[i]とかの場合前にxを掛ける)
-for i in range(1,n+1):
-  ans=0
-  for j in range(1,n//i+2):
-    if n-(i-1)*(j-1)>=j:
-      ans+=a[n-(i-1)*(j-1)]*b[j]%1000000007*b[n-(i-1)*(j-1)-j]%1000000007
-  print(ans%1000000007)
-    
+f = [1] * n
+inv = [1] * n
+for i in range(1, n):
+    f[i] = f[i-1] * i % mod
+    inv[i] = pow(f[i], mod-2, mod)
+ 
+ 
+def comb(n, k):
+    if k < 0 or k > n:
+        return 0
+    return f[n] * inv[n-k] % mod * inv[k] % mod
